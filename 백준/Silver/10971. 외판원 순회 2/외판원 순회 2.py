7884c85a581b23e15983821d023a450599067aa1
JIN HYUCK KIM <jh028323@gmail.com>
2025-03-19 23:05:40 +0900

    [Silver II] Title: 외판원 순회 2, Time: 112 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
+++ 백준/Silver/10971. 외판원 순회 2/외판원 순회 2.py
@@ -1,31 +1,31 @@
+import sys
+
 n = int(input())
-arr=[]
-for i in range(n):
-    arr.append(list(map(int, input().split())))
-m = []
-for i in range(n):
-    arr[i].append(i)
-def find(arr,select,depth,result):
-    if depth==n:
-        sum=0
-        for i in range(len(select)-1):
-            if select[i][select[i+1][-1]]!=0:
-                sum+=select[i][select[i+1][-1]]
-            else:
-                return None
-        if select[-1][select[0][-1]]!=0:
-            sum+=select[-1][select[0][-1]]
-        else:
-            return None
-        result.append(sum)
+arr = [list(map(int, input().split())) for _ in range(n)]
+visited = [False] * n  # 방문 체크 배열
+min_cost = float('inf')
+
+def tsp(start, now, cost, depth):
+    global min_cost
+    
+    if cost >= min_cost:  # 가지치기 (현재 비용이 이미 최소 비용보다 크면 중단)
         return
-    for i in range(len(arr)):
-        new_arr=arr[:i]+arr[i+1:]
-        select.append(arr[i])
-        find(new_arr,select,depth+1,result)
-        select.pop()
+    
+    if depth == n - 1:  # 모든 도시를 방문한 경우
+        if arr[now][start] != 0:  # 시작점으로 돌아갈 수 있을 때만 계산
+            min_cost = min(min_cost, cost + arr[now][start])
+        return
 
-        
+    for i in range(n):
+        if not visited[i] and arr[now][i] != 0:  # 방문하지 않았고 갈 수 있는 경로라면
+            visited[i] = True
+            tsp(start, i, cost + arr[now][i], depth + 1)
+            visited[i] = False
 
-find(arr, [], 0, m)
-print(min(m))
+# 모든 도시에서 시작해 보기
+for i in range(n):
+    visited[i] = True
+    tsp(i, i, 0, 0)
+    visited[i] = False
+
+print(min_cost)
