4fcf644492bbd9e5a78f36035c30c77f606d35f8
JIN HYUCK KIM <jh028323@gmail.com>
2025-03-22 10:41:19 +0900

    [Silver II] Title: 나무 자르기, Time: 2112 ms, Memory: 143532 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -1,22 +1,15 @@
-N, M = map(int, input().split())
-trees = list(map(int, input().split()))
-
-
-def solve(left, right):
-    best = 0
-    while left <= right:
-        
-        mid = (right + left)//2
-    
-        remains = sum(tree-mid for tree in trees if tree > mid)
-    
-        if remains < M:
-            right = mid-1
-        else :
-            best = mid
-            left = mid+1
-            
-    return best
-
-
-print(solve(0, max(trees)))
+N,M=map(int,input().split())
+trees=list(map(int,(input().split())))
+trees.sort()
+lo=0
+hi=trees[-1]
+def check(mid):
+    remains = sum(tree-mid for tree in trees if tree > mid)
+    return remains>=M   
+while lo+1<hi:
+    mid=(lo+hi)//2
+    if check(mid):
+        lo=mid
+    else:
+        hi=mid
+print(lo)
