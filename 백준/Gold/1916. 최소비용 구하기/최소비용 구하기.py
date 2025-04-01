ba8398659a9d2b4451af01587dd1f6547178651e
JIN HYUCK KIM <jh028323@gmail.com>
2025-04-01 19:25:19 +0900

    [Gold V] Title: 최소비용 구하기, Time: 280 ms, Memory: 121704 KB -BaekjoonHub

---
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -0,0 +1,37 @@
+from heapq import heappop, heappush
+
+def dijkstra(graph, start, end=None):
+    n = len(graph)
+    
+    heap = [(0,start)]
+    seen = set()
+    d = [float('inf')] * n
+    d[start] = 0
+    
+    while heap:
+        _, curr = heappop(heap)
+        if curr in seen:
+            continue
+        seen.add(curr)
+        for child, w in graph[curr]:
+            if d[curr] + w < d[child]:
+                d[child] = d[curr] + w
+                heappush(heap, (d[child], child))
+                
+    if end!=None:
+        return d[end]
+    return 0
+# graph 생성
+graph={}
+N=int(input())
+for i in range(N):
+    graph[i]=[]
+M=int(input())
+for _ in range(M):
+    a,b,c=map(int,input().split())
+    graph[a-1].append((b-1,c))
+    
+start,end=map(int,input().split())
+
+tmp=dijkstra(graph, start-1,end-1)
+print(tmp)
