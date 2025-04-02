55c4f74fbdb92d29d0cf433043404e7fbf962adb
JIN HYUCK KIM <jh028323@gmail.com>
2025-04-02 20:24:10 +0900

    [Gold III] Title: 아침 산책, Time: 804 ms, Memory: 105764 KB, Score: 200 point -BaekjoonHub

--- 백준/Gold/21606. 아침 산책/아침 산책.py
+++ 백준/Gold/21606. 아침 산책/아침 산책.py
@@ -1,41 +1,44 @@
 import sys
-sys.setrecursionlimit(100000)
-n=int(input())
-info=[0]+list(map(int,input()))
-graph={}
-for i in range(1,n+1):
-    graph[i]=[]
-for _ in range(n-1):
-    a,b=map(int,input().split())
-    graph[a].append(b)
-    graph[b].append(a)   
-    
-result=0
-for i in range(1,n+1):
-    if not info[i]==0:
-        for j in graph[i]:
-            if info[j]!=0:
-                result+=1
-                
-clusters=[]
-visited=[False]*(n+1)
-def dfs(node, cluster):
-    visited[node] = True
-    cluster.append(node)
-    for neighbor in graph[node]:
-        if not visited[neighbor] and info[neighbor] == 0:
-            dfs(neighbor, cluster)
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
 
+n = int(input())
+
+A = input().rstrip()
+
+graph = [[] for _ in range(n+1)]
+place = [0] * (n+1)
+visited = [0] * (n+1)
+
+for i in range(len(A)):
+    if A[i] == '1':
+        place[i+1] = 1
+
+for _ in range(n-1):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+    
+def dfs(node):
+    res = 0 
+    for next_node in graph[node]:
+        if place[next_node] == 0:
+            if not visited[next_node]:
+                visited[next_node] = 1
+                res += dfs(next_node)
+        else:
+            res += 1
+    return res
+
+ans = 0
 for i in range(1, n+1):
-    if info[i] == 0 and not visited[i]:
-        cluster = []
-        dfs(i, cluster)
-        clusters.append(cluster)
-for arr in clusters:
-    tmp=0
-    for i in arr:
-        for j in graph[i]:
-            if info[j]!=0:
-                tmp+=1
-    result+=tmp*(tmp-1)
-print(result)
+    if place[i] == 0:
+        if not visited[i]:
+            visited[i] = 1
+            res = dfs(i)
+            ans += res * (res - 1)
+    else:
+        for next_node in graph[i]:
+            if place[next_node] == 1:
+                ans += 1
+print(ans)
